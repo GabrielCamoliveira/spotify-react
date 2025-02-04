fa5b9bc929aacd640a3d09975c475ab32920367f
GabrielCamoliveira <gabriel.camoliveira@gmail.com>
2025-02-04 15:08:36 -0300

    Spotify React - commit 1

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import reset from './reset.css';
+import vars from './vars.css';
+import './App.css';
+import Sidebar_footer from './Sidebar_footer/Sidebar_footer';
+import Header from './Header/Header';
+
+function App() {
+  return (
+    <div>
+      <Sidebar_footer/>
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Sidebar_footer/Sidebar_footer.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Sidebar_footer.css";
+import logoSpotify from '../assets/icons/logo-spotify.png';
+
+const Sidebar_footer = () => {
+    return (
+        <div className="sidebar">
+            <nav className="sidebar_navigation">
+                <div className="logo">
+                    <a href="">
+                        <img src={logoSpotify} alt="Logo Spotify"/>
+                    </a>
+                </div>
+            
+                <ul>
+                    <li>
+                        <a href="">
+                            <span className="fa fa-home"></span>
+                            <span>Início</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="">
+                            <span className="fa fa-search"></span>
+                            <span>Buscar</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+            <div className="biblioteca">
+                <div className="biblioteca_content">
+                    <button className="biblioteca_button">
+                        <span className="fa fas fa-book"></span>
+                        <span>Sua Biblioteca</span>
+                    </button>
+                    <span className="fa fa-plus"></span>
+                </div>
+
+                <section className="sectionPlaylist">
+                    <div className="sectionPlaylist_content">
+                        <span className="text title">Crie sua primeira playlist</span>
+                        <span className="text subtitle">É fácil, vamos te ajudar</span>
+                        <button className="sectionPlaylist_button">
+                            <span>Criar playlist</span>
+                        </button>
+                    </div>
+                </section>
+
+                <div className="cookies">
+                    <a href="">Cookies</a>
+                </div>
+
+                <div className="languages">
+                    <button className="languages_button">
+                        <span className="fa fa-globe"></span>
+                        <span>Português do Brasil</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default Sidebar_footer;
